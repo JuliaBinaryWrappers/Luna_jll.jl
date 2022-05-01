74a804da057d62123d7e1edd35a64075f3379c3a
jlbuild <juliabuildbot@gmail.com>
2022-05-01 04:39:44 +0000

    Luna_jll build 0.26.2+0

--- src/wrappers/i686-w64-mingw32-cxx11.jl
+++ src/wrappers/i686-w64-mingw32-cxx11.jl
@@ -3,90 +3,21 @@ export libluna, luna
 
 using FFTW_jll
 using Zlib_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-
-# Relative path to `libluna`
-const libluna_splitpath = ["bin", "libluna.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libluna_path = ""
-
-# libluna-specific global declaration
-# This will be filled out by __init__()
-libluna_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libluna = "libluna.dll"
-
-
-# Relative path to `luna`
-const luna_splitpath = ["bin", "luna.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-luna_path = ""
-
-# luna-specific global declaration
-function luna(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        if !isempty(get(ENV, LIBPATH_env, ""))
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', ENV[LIBPATH_env])
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(luna_path)
-    end
-end
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Luna")
+JLLWrappers.@declare_library_product(libluna, "libluna.dll")
+JLLWrappers.@declare_executable_product(luna)
 function __init__()
-    global artifact_dir = abspath(artifact"Luna")
+    JLLWrappers.@generate_init_header(FFTW_jll, Zlib_jll)
+    JLLWrappers.@init_library_product(
+        libluna,
+        "bin\\libluna.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (FFTW_jll.PATH_list, Zlib_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (FFTW_jll.LIBPATH_list, Zlib_jll.LIBPATH_list,))
+    JLLWrappers.@init_executable_product(
+        luna,
+        "bin\\luna.exe",
+    )
 
-    global libluna_path = normpath(joinpath(artifact_dir, libluna_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libluna_handle = dlopen(libluna_path)
-    push!(LIBPATH_list, dirname(libluna_path))
-
-    global luna_path = normpath(joinpath(artifact_dir, luna_splitpath...))
-
-    push!(PATH_list, dirname(luna_path))
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(LIBPATH_list, ';')
-
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
